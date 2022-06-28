3fa35ac093892cf069f7be032da4fa9e36a814cb
yanielajanis <yanielajanis@gmail.com>
2022-06-28 18:24:47 -0400

    modifiying main with api definition

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import requests
+from flask import Flask, request
 import pickle
 from google.cloud import storage
 import numpy as np
@@ -9,20 +9,21 @@ blob = bucket.blob('flowers-pkl')
 blob.download_to_filename('/tmp/flowers-pkl')
 model_pk = pickle.load(open('/tmp/flowers-pkl', 'rb'))
 
-def api_predict(request):
-    if request.method == 'GET':
-        return "Please Send POST Request"
-    elif request.method == 'POST':
-        
-        data = request.get_json()
-        
-        sepal_length = data["sepal_length"]
-        sepal_width = data["sepal_width"]
-        petal_length = data["petal_length"]
-        petal_width = data["petal_width"]
+
+app=Flask(__name__)
+
+#http:localhost:5000/api_predict
+@app.route("/api_predict", methods=["POST","GET"])
+def api_predict():
+    if request.method=="POST":
+       data=request.get_json()
+       sepal_length=data['sepal.length']
+       sepal_width=data['sepal.width']
+       petal_length=data['petal.length']
+       petal_width=data['petal.width']
+       input_data=np.array([[sepal_length,sepal_width,petal_length,petal_width]])
+       prediction=model_pk.predict(input_data) 
+       return str(prediction)
+    else: return "Please send a POST request"   
     
-        data = np.array([[sepal_length, sepal_width, 
-                          petal_length, petal_width]])
-           
-        prediction = model_pk.predict(data)
-        return str(prediction)
+app.run()
