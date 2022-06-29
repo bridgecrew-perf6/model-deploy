399347ff0b5972fd838af855a9dbf0d1ac262d8f
yanielajanis <yanielajanis@gmail.com>
2022-06-29 09:10:53 -0400

    version2

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 from flask import Flask, request
 import pickle
-#from google.cloud import storage
 import numpy as np
+from sklearn.linear_model import LogisticRegression
 
-#storage_client = storage.Client()
-#bucket = storage_client.get_bucket('model-bucket-iris-yaniela')
-#blob = bucket.blob('flowers-pkl')
-#blob.download_to_filename('flowers-pkl')
+
+
 model_pk = pickle.load(open('flowers-pkl', 'rb'))
-
 
 app=Flask(__name__)
 
